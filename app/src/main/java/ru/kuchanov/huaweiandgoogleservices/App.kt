964f440fd76d96d48c8dfcf9bb3c4c26a348f712
mohax <mohax.spb@gmail.com>
2021-06-03 15:25:27 +0300

    Init Timber.

--- app/src/main/java/ru/kuchanov/huaweiandgoogleservices/App.kt
+++ app/src/main/java/ru/kuchanov/huaweiandgoogleservices/App.kt
@@ -5,11 +5,14 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.kuchanov.huaweiandgoogleservices.di.AppModule
 import ru.kuchanov.huaweiandgoogleservices.di.SystemModule
+import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
 
         startKoin {
             androidContext(this@App)
